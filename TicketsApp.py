37c5647ba39db1e874e75873d23100894da00daf
Natalia Gerasimova <nataliagerasimova@MBP-PGE.local>
2025-09-19 13:45:00 -0400

    Almost final updates

--- TicketsApp.py
+++ TicketsApp.py
@@ -1,4 +1,6 @@
-print("Welcome to our place. We have 3 types of tickets: for our little guests, adults and seniors.")
+kids_price = int(16) 
+adult_price = int(32)
+senior_price = int(25)
 def validate_user_input(age: str) -> bool:
     """
     Check the user input if it is in digit form.
@@ -13,11 +15,11 @@ def ticket_price(the_age):
     Determine the price for the ticket based on the user age.
     """
     if the_age < 14:
-        return(f"Ticket price is ${16}")
+        return("Ticket price is $16")
     elif the_age < 60:
-        return(f"Ticket price is ${32}")
+        return("Ticket price is $32")
     else:
-        return(f"Ticket price is ${25}")    
+        return("Ticket price is $25")    
 
 def agrement_to_buy(message: str) -> bool:
     if message == "y":
@@ -26,7 +28,7 @@ def agrement_to_buy(message: str) -> bool:
         print("See you next time.")
         return False
     
-def collecting_user_information(message_2):
+def collecting_user_information(message_2: int) -> int:
     """
     Collecting information - how many tickets the user chose.
     """
@@ -41,25 +43,26 @@ def user_ticket_number_input(message_3):
     """
     Requst for user to input tickets number.
     """
-    a = input(f"Please, choose the number of {message_3}:  ").strip()
-    return a
+    z = input(f"Please, choose the number of {message_3}:  ").strip()
+    return z
 
+
+print("Welcome to our place. We have 3 types of tickets:"
+" for our little guests, adults and seniors.")
 while True:
     user_age = input("Please, provide your age:  ").strip()
     if validate_user_input(user_age):
         break
-
 user_age_int = int(user_age)
 one_ticket_price = ticket_price(user_age_int)
 print(one_ticket_price)
 agrement_to_buy(input("Do you want to buy tickets now? Please, choose Y or N:  ").strip())
 
 kids_tickets = user_ticket_number_input("kids tickets")
-collecting_user_information(kids_tickets)
+a = collecting_user_information(kids_tickets)
 adult_tickets = user_ticket_number_input("adult tickets")
-collecting_user_information(adult_tickets)
+b = collecting_user_information(adult_tickets)
 senior_tickets = user_ticket_number_input("senior tickets")
-collecting_user_information(senior_tickets)
-
-
-#def determine_final_price()
+c = collecting_user_information(senior_tickets)
+total_price = a * kids_price + b * adult_price + c * senior_price
+print(f"Your total price is: {total_price}")
